cdfc50b3882d0df0fe9cb3b40fc3c93ece548c5d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-09-20 00:15:51 +0000

    start testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { MessageModule } from './message/message.module';
+import { ConversationModule } from './conversation/conversation.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
     TasksModule,
-    AuthModule
+    AuthModule,
+    MessageModule,
+    ConversationModule,
+    ChatModule
   ],
 })
 export class AppModule {}
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,30 +1,31 @@
-import { Test } from "@nestjs/testing";
-import { TasksService } from "./tasks.service";
-import { TasksRepository } from "./task.repository";
+// import { Test } from "@nestjs/testing";
+// import { TasksService } from "./tasks.service";
+// import { TasksRepository } from "./task.repository";
 
-const mockTaskRepository = () => ({
-   getTasks: jest.fn(),
-});
+// const mockTaskRepository = () => ({
+//    getTasks: jest.fn(),
+// });
 
-describe("TaskService", () => {
-   let tasksService: TasksService;
-   let taskRepository: TasksRepository;
+// describe("TaskService", () => {
+//    let tasksService: TasksService;
+//    let taskRepository: TasksRepository;
 
-   beforeEach(async () => {
-      const module = await Test.createTestingModule({
-         providers: [
-            TasksService,
-            {provide: TasksRepository, useFactory: mockTaskRepository}
-         ]
-      }).compile();
+//    beforeEach(async () => {
+//       const module = await Test.createTestingModule({
+//          providers: [
+//             TasksService,
+//             { provide: TasksRepository, useFactory: mockTaskRepository }
+//          ]
+//       }).compile();
 
-      tasksService =  module.get<TasksService>(TasksService);
-      taskRepository =  module.get<TasksRepository>(TasksRepository);
+//       tasksService = module.get<TasksService>(TasksService);
+//       taskRepository = module.get<TasksRepository>(TasksRepository);
 
-      describe('getTasks', () => {
-         it('gets all tasks  from the repository', ()=> {
-            expect(taskRepository.getTasks).not.toHaveBeenCalled();
-         })
-      })
-   })
-})
+//       describe('getTasks', () => {
+//          it('gets all tasks  from the repository', () => {
+//             expect(taskRepository.getTasks).not.toHaveBeenCalled();
+//          });
+//       });
+//    });
+// });
+
